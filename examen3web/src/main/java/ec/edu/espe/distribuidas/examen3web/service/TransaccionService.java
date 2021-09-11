9190835d98ecb407659f8406ec091b7f4b5e1f0d
Jazz <yazbel2009@hotmail.com>
2021-09-10 21:34:58 -0500

    Correccion

--- examen3web/src/main/java/ec/edu/espe/distribuidas/examen3web/service/TransaccionService.java
+++ examen3web/src/main/java/ec/edu/espe/distribuidas/examen3web/service/TransaccionService.java
@@ -42,37 +42,36 @@ public class TransaccionService {
 
         if ("RET".equals(transaccion.getTipo())) {
             if (cajero.get().getMontoTotal().compareTo(transaccion.getMontoTransaccion()) == 1) {
-               
+
                 BigDecimal dinero10 = new BigDecimal(transaccion.getBilletes10() * 10);
                 BigDecimal dinero20 = new BigDecimal(transaccion.getBilletes20() * 20);
                 BigDecimal total = dinero10.add(dinero20);
                 if (total.compareTo(transaccion.getMontoTransaccion()) == 0) {
-                
+
                     cajero.get().setMontoTotal(cajero.get().getMontoTotal().subtract(transaccion.getMontoTransaccion()));
                     cajero.get().setTotalBilletes10(cajero.get().getTotalBilletes10() - transaccion.getBilletes10());
                     cajero.get().setTotalBilletes20(cajero.get().getTotalBilletes20() - transaccion.getBilletes20());
-                    if(cajero.get().getTotalBilletes10()<0 || cajero.get().getTotalBilletes20()<0 ){
+                    if (cajero.get().getTotalBilletes10() < 0 || cajero.get().getTotalBilletes20() < 0) {
                         throw new CreateException("No existen billetes sufucientes");
                     }
                     this.transaccionRepo.save(transaccion);
                     this.cajeroRepo.save(cajero.get());
-                 }
+                }
             }
         }
-            if ("DEP".equals(transaccion.getTipo())) {
-                BigDecimal dinero10 = new BigDecimal(transaccion.getBilletes10() * 10);
-                BigDecimal dinero20 = new BigDecimal(transaccion.getBilletes20() * 20);
-                BigDecimal total = dinero10.add(dinero20);
-                if (total.compareTo(transaccion.getMontoTransaccion()) == 0) {
-                    this.transaccionRepo.save(transaccion);
-                    cajero.get().setMontoTotal(cajero.get().getMontoTotal().add(transaccion.getMontoTransaccion()));
-                    cajero.get().setTotalBilletes10(cajero.get().getTotalBilletes10() + transaccion.getBilletes10());
-                    cajero.get().setTotalBilletes20(cajero.get().getTotalBilletes20() + transaccion.getBilletes20());
-                    this.cajeroRepo.save(cajero.get());
-                }
-
+        if ("DEP".equals(transaccion.getTipo())) {
+            BigDecimal dinero10 = new BigDecimal(transaccion.getBilletes10() * 10);
+            BigDecimal dinero20 = new BigDecimal(transaccion.getBilletes20() * 20);
+            BigDecimal total = dinero10.add(dinero20);
+            if (total.compareTo(transaccion.getMontoTransaccion()) == 0) {
+                this.transaccionRepo.save(transaccion);
+                cajero.get().setMontoTotal(cajero.get().getMontoTotal().add(transaccion.getMontoTransaccion()));
+                cajero.get().setTotalBilletes10(cajero.get().getTotalBilletes10() + transaccion.getBilletes10());
+                cajero.get().setTotalBilletes20(cajero.get().getTotalBilletes20() + transaccion.getBilletes20());
+                this.cajeroRepo.save(cajero.get());
             }
 
-        
+        }
+
     }
 }
